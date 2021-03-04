024c64e706b81253eebbc9266fd8bb12fa6f98b8
DESKTOP-RVPFQJ7\ttobo <ttobokk@gmail.com>
2021-03-04 22:36:16 +0900

    Second Commit

---
+++ 210304_1.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+        public static void main(string[] args) {
+            System.out.println("안녕하세요?");
+            System.out.println("JAVA입니다.");        
+        }
+}
---
+++ 210304_2.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+        public static void main(String[] args) {
+
+            System.out.println("이클립스 IDE");
+            System.out.println("처음 만들어보는 Java 클래스");
+            System.out.println();
+        }
+        
+}
