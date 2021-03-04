a01fde52ef212e90db1a9ffa78e63c4a64838c97
DESKTOP-RVPFQJ7\ttobo <ttobokk@gmail.com>
2021-03-04 22:43:27 +0900

    Third commit

--- 210304_1.java
+++ 210304_1.java
@@ -1,5 +1,5 @@
 class HelloWorld {
-        public static void main(string[] args) {
+        public static void main(String[] args){
             System.out.println("안녕하세요?");
             System.out.println("JAVA입니다.");        
         }
